e5650ec4df4311023b4d068a0f95e054a035d8fc
Brendon Tse and Kira Roberts <kira@>
2019-10-01 15:09:56 -0700

    add GetAll test

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -6,8 +6,12 @@ using ToDoList.Models;
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTest
+  public class ItemTest : IDisposable
   {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
 
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
@@ -46,6 +50,21 @@ namespace ToDoList.Tests
     {
       //Arrange
       List<Item> newList = new List<Item> { };
+      //Act
+      List<Item> result = Item.GetAll();
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturndItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
       //Act
       List<Item> result = Item.GetAll();
       //Assert
