69a290166f2b8d905215372a92e08ffa29451245
Brendon Tse and Kira Roberts <brendon@>
2019-10-01 11:08:20 -0700

    add basic file structure, logic, and first tests, gitignore

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTest
+  {
+
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+        Item newItem = new Item("test");
+        Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+      Assert.AreEqual(description, result);
+    }
+  }
+}
---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,11 @@
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public string Description { get; }
+    public Item(string description)
+    {
+      Description = description;
+    }
+  }
+}
