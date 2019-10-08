f5ddd5b9445d276580618504a090c09950b352f6
Carrie Schmid and Kira Roberts <example@example.com>
2019-10-08 10:04:16 -0700

    Add MVC forms, foreach loop and new controller file for Items.

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,44 +1,20 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ToDoList
 {
-    class ToDoList
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        static void Main()
-        {
-            Console.WriteLine("--------------------------");
-            Console.WriteLine("What would you like to do today? (Add/View)");
-            Console.WriteLine("--------------------------");
-            string userAction = Console.ReadLine();
-            if (userAction == "Add")
-            {
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("What task are you adding to your To-Do List?");
-                Console.WriteLine("--------------------------");
-                string userTask = Console.ReadLine();
-                Item newTask = new Item(userTask);
-            }
-            else if (userAction == "View")
-            {
-                int listCount = Item.GetAll().Count;
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-                if (listCount == 0)
-                {
-                    Console.WriteLine("You have no tasks in your To-Do List!");
-                } 
-                else 
-                {
-                    var userList = Item.GetAll();
-                    Console.WriteLine(userList);
-                }
-            }
-            else 
-            {
-                Console.WriteLine("Please enter a valid option. (Add/View)");
-            }
-        }    
+      host.Run();
     }
-
+  }
 }
